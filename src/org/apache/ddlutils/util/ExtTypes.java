3b2f13976e999c0a02eee79b3aa53208315608b6
Adrián Romero <adrian.romero@openbravo.com>
2008-01-23 18:53:12 +0000

    Project name changes
    --HG--
    extra : convert_revision : svn%3A6fd5776a-9540-455a-916b-3609c81e8f66/trunk%402

--- src/org/apache/ddlutils/util/ExtTypes.java
+++ src/org/apache/ddlutils/util/ExtTypes.java
@@ -1,11 +1,14 @@
 /*
- * ExtTypes.java
- *
- * Created on 16 de octubre de 2007, 19:25
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
+************************************************************************************
+* Copyright (C) 2001-2006 Openbravo S.L.
+* Licensed under the Apache Software License version 2.0
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to  in writing,  software  distributed
+* under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+* CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+* specific language governing permissions and limitations under the License.
+************************************************************************************
+*/
 
 package org.apache.ddlutils.util;
 
