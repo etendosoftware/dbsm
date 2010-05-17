1ca9c4f4c9897c37350d4d71a789a67cdc16ca89
Antonio Moreno <antonio.moreno@openbravo.com>
2010-05-17 16:34:57 +0200

    Reverted fix for issue 13030. To avoid any kind of raising false positive errors in the build, we will fix this in a different manner. A separate issue for this has been created with id=13340

--- src/org/apache/ddlutils/platform/PlatformImplBase.java
+++ src/org/apache/ddlutils/platform/PlatformImplBase.java
@@ -291,11 +291,6 @@ public abstract class PlatformImplBase extends JdbcSupport implements Platform {
    */
   public int evaluateBatch(Connection connection, String sql, boolean continueOnError)
       throws DatabaseOperationException {
-    return evaluateBatch(connection, sql, continueOnError, false);
-  }
-
-  public int evaluateBatch(Connection connection, String sql, boolean continueOnError,
-      boolean logAsError) throws DatabaseOperationException {
     Statement statement = null;
     int errors = 0;
     int commandCount = 0;
@@ -346,14 +341,8 @@ public abstract class PlatformImplBase extends JdbcSupport implements Platform {
             // on level warn, and the exception itself on level
             // debug
             if (!command.contains("SCRIPT OPTIONS (FORCE = TRUE)")) {
-              String message = "SQL Command failed with: " + ex.getMessage();
-              if (logAsError) {
-                _log.error(message);
-                _log.error(command);
-              } else {
-                _log.warn(message);
-                _log.warn(command);
-              }
+              _log.warn("SQL Command failed with: " + ex.getMessage());
+              _log.warn(command);
               if (_log.isDebugEnabled()) {
                 _log.debug(ex);
               }
@@ -409,10 +398,8 @@ public abstract class PlatformImplBase extends JdbcSupport implements Platform {
             }
             it.remove();
           } catch (SQLException ex) {
-            if (loops == MAX_LOOPS_OF_FORCED) {
-              _log.info("SQL Command failed with: " + ex.getMessage());
-              _log.info(command);
-            }
+
+            _log.warn("SQL Command failed with: " + ex.getMessage());
             if (_log.isDebugEnabled()) {
               _log.debug(ex);
             }
@@ -429,13 +416,8 @@ public abstract class PlatformImplBase extends JdbcSupport implements Platform {
       }
 
       if (!aForcedCommands.isEmpty()) {
-        String message = "There are still " + aForcedCommands.size()
-            + " forced commands not executed sucessfully.";
-        if (logAsError) {
-          _log.error(message);
-        } else {
-          _log.info(message);
-        }
+        _log.info("There are still " + aForcedCommands.size()
+            + " forced commands not executed sucessfully.");
       }
 
     } catch (SQLException ex) {
@@ -683,7 +665,7 @@ public abstract class PlatformImplBase extends JdbcSupport implements Platform {
       // won't happen because we're using a string writer
     }
 
-    evaluateBatch(connection, sql, continueOnError, true);
+    evaluateBatch(connection, sql, continueOnError);
   }
 
   public void alterTables(Database currentModel, Database desiredModel, boolean continueOnError,
