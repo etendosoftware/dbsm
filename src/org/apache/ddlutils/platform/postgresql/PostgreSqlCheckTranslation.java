9f965bd4e648604b33f012be70bf4202977c1902
Asier Lostalé <asier.lostale@openbravo.com>
2015-01-20 15:49:56 +0100

    fixed bug 28684: incorrect check constraint export in pg 9.3 and 9.4
      The differences appear for check constraints in character varying columns,
      restoring DB adds extra casting in this case.
    
      The fix takes care of these cases.

--- src/org/apache/ddlutils/platform/postgresql/PostgreSqlCheckTranslation.java
+++ src/org/apache/ddlutils/platform/postgresql/PostgreSqlCheckTranslation.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2001-2006 Openbravo S.L.U.
+ * Copyright (C) 2001-2015 Openbravo S.L.U.
  * Licensed under the Apache Software License version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to  in writing,  software  distributed
@@ -24,6 +24,8 @@ import org.apache.ddlutils.translation.Translation;
 public class PostgreSqlCheckTranslation extends CombinedTranslation {
 
   public PostgreSqlCheckTranslation() {
+    // pg 9.3+ adds extra casting to character varying columns, let's remove them
+    append(new ReplacePatTranslation("\\(([^\\(]*)(::character varying)\\)::text", "$1$2"));
 
     append(new ReplaceStrTranslation(" = ANY", " in"));
     append(new ReplaceStrTranslation("ARRAY[", ""));
@@ -31,9 +33,17 @@ public class PostgreSqlCheckTranslation extends CombinedTranslation {
     append(new ReplaceStrTranslation("::bpchar", ""));
     append(new ReplaceStrTranslation("::text", ""));
     append(new ReplaceStrTranslation("::character varying", ""));
-    append(new ReplacePatTranslation("\\(([0-9\\.\\-]+?)\\)::[Nn][Uu][Mm][Ee][Rr][Ii][Cc]", "$1"));
-    append(new ReplacePatTranslation("\\((.*)\\) in \\((.*)\\)", "$1 IN $2"));
+
     append(new ReplacePatTranslation("\\[", ""));
+
+    append(new ReplacePatTranslation("\\(([0-9\\.\\-]+?)\\)::[Nn][Uu][Mm][Ee][Rr][Ii][Cc]", "$1"));
+
+    // remove 1st double brackets if present
+    append(new ReplacePatTranslation("\\((\\(.+)\\)( in .*)", "$1$2"));
+
+    // remove 2nd double brackets if present
+    append(new ReplacePatTranslation("(.*) in \\((\\(.+\\))\\)", "$1 IN $2"));
+
     append(new Translation() {
       public String exec(String s) {
         return s.substring(1, s.length() - 1).toUpperCase();
