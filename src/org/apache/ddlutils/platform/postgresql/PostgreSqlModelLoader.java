db568b4a7219177c09ecadbf9250797f1ad98c00
Asier Lostalé <asier.lostale@openbravo.com>
2015-01-23 08:19:31 +0100

    [seq] pg: read sequences in 2 steps
      1st one reads sequences from catalog
      2nd one reads details start number and increment from the sequence itself

--- src/org/apache/ddlutils/platform/postgresql/PostgreSqlModelLoader.java
+++ src/org/apache/ddlutils/platform/postgresql/PostgreSqlModelLoader.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2001-2006 Openbravo S.L.U.
+ * Copyright (C) 2001-2015 Openbravo S.L.U.
  * Licensed under the Apache Software License version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to  in writing,  software  distributed
@@ -18,6 +18,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -31,6 +32,7 @@ import org.apache.ddlutils.model.Function;
 import org.apache.ddlutils.model.Index;
 import org.apache.ddlutils.model.IndexColumn;
 import org.apache.ddlutils.model.Parameter;
+import org.apache.ddlutils.model.Sequence;
 import org.apache.ddlutils.model.Table;
 import org.apache.ddlutils.model.Trigger;
 import org.apache.ddlutils.model.Unique;
@@ -133,7 +135,7 @@ public class PostgreSqlModelLoader extends ModelLoaderBase {
       String standardizedBody = viewStandarization.exec(body);
       if (standardizedBody.endsWith("\n"))
         standardizedBody = standardizedBody.substring(0, standardizedBody.length() - 1);
-      standardizedBody=standardizedBody.trim();
+      standardizedBody = standardizedBody.trim();
       db.getView(i).setStatement(standardizedBody);
     }
 
@@ -339,6 +341,8 @@ public class PostgreSqlModelLoader extends ModelLoaderBase {
     }
     _stmt_listviews = _connection.prepareStatement(sql);
 
+    // sequences are partially loaded from catalog, details (start number and increment) are read by
+    // readSequences method
     if (_filter.getExcludedSequences().length == 0) {
       sql = "SELECT upper(relname), 1, 1 FROM pg_class WHERE relkind = 'S'";
     } else {
@@ -736,6 +740,36 @@ public class PostgreSqlModelLoader extends ModelLoaderBase {
     }
 
     return t;
+  }
+
+  @SuppressWarnings("rawtypes")
+  @Override
+  protected Collection readSequences() throws SQLException {
+    return readList(_stmt_listsequences, new RowConstructor() {
+      public Object getRow(ResultSet r) throws SQLException {
+        _log.debug("Sequence " + r.getString(1));
+        final Sequence sequence = new Sequence();
+        String sequenceName = r.getString(1);
+        sequence.setName(sequenceName);
+
+        // PG does not store sequence details in catalog but in a pseudo-table named with the
+        // sequence name, let's query them to get the details
+        String sql = "select min_value, increment_by from " + sequenceName;
+        PreparedStatement stmtCurrentSequence = _connection.prepareStatement(sql);
+        readList(stmtCurrentSequence, new RowConstructor() {
+          @Override
+          public Object getRow(ResultSet seqDetails) throws SQLException {
+            sequence.setStart(seqDetails.getInt(1));
+            sequence.setIncrement(seqDetails.getInt(2));
+            return sequence;
+          }
+        });
+
+        stmtCurrentSequence.close();
+
+        return sequence;
+      }
+    });
   }
 
   protected Integer[] getIntArray(ResultSet r, int iposition) throws SQLException {
