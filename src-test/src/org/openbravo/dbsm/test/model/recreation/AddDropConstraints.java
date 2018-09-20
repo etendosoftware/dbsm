12706b474490000793786faf0fb18eb48bf6bb9d
Carlos Aristu <carlos.aristu@openbravo.com>
2018-09-20 18:52:18 +0200

    related to issue 39273: PG 11 - NPE updating DB
       Fixes test case in PostgreSQL 11.
    
     Ensure extensions are created and dropped as system use, otherways PG 11
     doesn't allow it.

--- src-test/src/org/openbravo/dbsm/test/model/recreation/AddDropConstraints.java
+++ src-test/src/org/openbravo/dbsm/test/model/recreation/AddDropConstraints.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015 Openbravo S.L.U.
+ * Copyright (C) 2015-2018 Openbravo S.L.U.
  * Licensed under the Apache Software License version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to  in writing,  software  distributed
@@ -33,6 +33,16 @@ public class AddDropConstraints extends TableRecreationBaseTest {
       String password, String name, ActionType type, DbsmTest.RecreationMode recMode)
       throws FileNotFoundException, IOException {
     super(rdbms, driver, url, sid, user, password, name, type, recMode);
+  }
+
+  @Override
+  protected String getSystemUser() {
+    return "postgres";
+  }
+
+  @Override
+  protected String getSystemPassword() {
+    return "postgres";
   }
 
   @Parameters(name = "DB: {6} - {7} - recreation {8}")
