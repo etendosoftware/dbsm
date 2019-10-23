8d328e137bf224ae6a791660fb2a628bbc81cdcb
Augusto Mauch <augusto.mauch@openbravo.com>
2019-10-23 10:52:22 +0200

    [otherdbmodel] Refactors CreateDatabase to make it extensible
    The CreateDatabase task has been refactored:
    - preCreateModel: execution of prescripts
    - createMode: creation of the database model
    - postCreateModel: postscripts, checksum update, insertion of source data
    
    preCreateModel and postCreateModel are protected methods, this will allow to create a class that
    extends it and that only creates the database model.

--- src/org/openbravo/ddlutils/task/CreateDatabase.java
+++ src/org/openbravo/ddlutils/task/CreateDatabase.java
@@ -64,170 +64,98 @@ public class CreateDatabase extends BaseDatabaseTask {
         + getSystemUser());
     final Platform platform = getPlatformInstance();
     try {
-      executeSystemPreScript(platform);
-      executePreScript(platform);
-      Database db = null;
-      if (modulesDir == null) {
-        getLog().info(
-            "modulesDir for additional files not specified. Creating database with just Core.");
-        db = DatabaseUtils.readDatabaseWithoutConfigScript(getModel());
-      } else {
-        // We read model files using the filter, obtaining a file array. The models will be merged
-        // to create a final target model.
-        final Vector<File> dirs = new Vector<File>();
-        dirs.add(model);
-        final DirectoryScanner dirScanner = new DirectoryScanner();
-        dirScanner.setBasedir(new File(modulesDir));
-        final String[] dirFilterA = { dirFilter };
-        dirScanner.setIncludes(dirFilterA);
-        dirScanner.scan();
-        final String[] incDirs = dirScanner.getIncludedDirectories();
-        for (int j = 0; j < incDirs.length; j++) {
-          final File dirF = new File(modulesDir, incDirs[j]);
-          dirs.add(dirF);
-        }
-        final File[] fileArray = new File[dirs.size()];
-        for (int i = 0; i < dirs.size(); i++) {
-          fileArray[i] = dirs.get(i);
-        }
 
-        db = DatabaseUtils.readDatabaseWithoutConfigScript(fileArray);
-      }
+      preCreateModel(platform);
 
-      // Create database
-      getLog().info("Executing creation script");
-      // crop database if needed
-      if (object != null) {
-        final Database empty = new Database();
-        empty.setName("empty");
-        db = DatabaseUtils.cropDatabase(empty, db, object);
-        getLog().info("for database object " + object);
-      } else {
-        getLog().info("for the complete database");
-      }
+      Database db = createModel(platform);
 
-      if (!platform.createTables(db, isDropfirst(), !isFailonerror())) {
-        throw new Exception(MSG_ERROR);
-      }
-
-      // execute the post-script
-      if (getPostscript() == null) {
-        // try to execute the default prescript
-        final File fpost = new File(getModel(), "postscript-" + platform.getName() + ".sql");
-        if (fpost.exists()) {
-          getLog().info("Executing default postscript");
-          platform.evaluateBatch(DatabaseUtils.readFile(fpost), !isFailonerror());
-        }
-      } else {
-        platform.evaluateBatch(DatabaseUtils.readFile(getPostscript()), !isFailonerror());
-      }
-
-      getLog().info("Writing checksum info");
-      DBSMOBUtil
-          .writeCheckSumInfo(new File(model.getAbsolutePath() + "/../../../").getAbsolutePath());
-
-      // Now we insert sourcedata into the database first we load the data files
-      final String folders = getInput();
-      final StringTokenizer strTokFol = new StringTokenizer(folders, ",");
-      final Vector<File> files = new Vector<File>();
-      while (strTokFol.hasMoreElements()) {
-        if (basedir == null) {
-          getLog().info("Basedir not specified, will insert just Core data files.");
-          final String folder = strTokFol.nextToken();
-          final File[] fileArray = DatabaseUtils.readFileArray(new File(folder));
-          for (int i = 0; i < fileArray.length; i++) {
-            files.add(fileArray[i]);
-          }
-        } else {
-          final String token = strTokFol.nextToken();
-          final DirectoryScanner dirScanner = new DirectoryScanner();
-          dirScanner.setBasedir(new File(basedir));
-          final String[] dirFilterA = { token };
-          dirScanner.setIncludes(dirFilterA);
-          dirScanner.scan();
-          final String[] incDirs = dirScanner.getIncludedDirectories();
-          for (int j = 0; j < incDirs.length; j++) {
-            final File dirFolder = new File(basedir, incDirs[j] + "/");
-            final File[] fileArray = DatabaseUtils.readFileArray(dirFolder);
-            for (int i = 0; i < fileArray.length; i++) {
-              files.add(fileArray[i]);
-            }
-          }
-        }
-      }
-
-      getLog().info("Disabling triggers");
-      Connection _connection = platform.borrowConnection();
-      platform.disableAllTriggers(_connection, db, false);
-      platform.returnConnection(_connection);
-
-      getLog().info("Inserting data into the database.");
-      // Now we insert the data into the database
-      final DatabaseDataIO dbdio = new DatabaseDataIO();
-      dbdio.setEnsureFKOrder(false);
-      dbdio.setUseBatchMode(true);
-      DataReader dataReader = dbdio.getConfiguredDataReader(platform, db);
-      dataReader.getSink().start();
-
-      for (int i = 0; i < files.size(); i++) {
-        getLog().debug("Importing data from file: " + files.get(i).getName());
-        dbdio.writeDataToDatabase(dataReader, files.get(i));
-      }
-
-      /*
-       * end method does final inserts when using batching, so call it after importing data files
-       * but before doing anything using those data (template, fk's, not null, ... )
-       */
-      dataReader.getSink().end();
-
-      DatabaseData dbData = new DatabaseData(db);
-      DatabaseUtils.readDataModuleInfo(db, dbData, basedir);
-      for (String template : DBSMOBUtil.getInstance().getSortedTemplates(dbData)) {
-        File configScript = new File(new File(modulesDir),
-            template + "/src-db/database/configScript.xml");
-        getLog()
-            .info("Loading config script for module from path " + configScript.getAbsolutePath());
-        if (configScript.exists()) {
-          final DatabaseIO dbIO = new DatabaseIO();
-          final Vector<Change> changesConfigScript = dbIO.readChanges(configScript);
-          platform.applyConfigScript(db, changesConfigScript);
-        } else {
-          getLog().error("Error. We couldn't find configuration script for template "
-              + configScript.getName() + ". Path: " + configScript.getAbsolutePath());
-        }
-      }
-
-      getLog().info("Executing onCreateDefault statements");
-      platform.executeOnCreateDefaultForMandatoryColumns(db, null);
-      getLog().info("Enabling notnull constraints");
-      platform.enableNOTNULLColumns(db);
-
-      boolean continueOnError = false;
-      getLog().info("Creating foreign keys");
-      boolean fksEnabled = platform.createAllFKs(db, continueOnError);
-
-      _connection = platform.borrowConnection();
-      getLog().info("Enabling triggers");
-      boolean triggersEnabled = platform.enableAllTriggers(_connection, db, false);
-      platform.returnConnection(_connection);
-
-      executePostScript(platform);
-
-      if (!triggersEnabled) {
-        getLog().error(
-            "Not all the triggers were correctly activated. The most likely cause of this is that the XML file of the trigger is not correct.");
-      }
-      if (!fksEnabled) {
-        getLog().error(
-            "Not all the foreign keys were correctly activated. Please review which ones were not, and fix the missing references.");
-      }
-      if (!triggersEnabled || !fksEnabled) {
-        throw new Exception(MSG_ERROR);
-      }
+      postCreateModel(platform, db);
 
     } catch (final Exception e) {
       getLog().error("Error creating database", e);
       throw new BuildException(e);
+    }
+  }
+
+  protected void preCreateModel(final Platform platform) throws IOException {
+    executePrescripts(platform);
+  }
+
+  private void postCreateModel(final Platform platform, Database db) throws IOException, Exception {
+    executePostscript(platform);
+    writeChecksumInfo();
+    insertSourceData(platform, db);
+  }
+
+  private void writeChecksumInfo() {
+    getLog().info("Writing checksum info");
+    DBSMOBUtil
+        .writeCheckSumInfo(new File(model.getAbsolutePath() + "/../../../").getAbsolutePath());
+  }
+
+  private Database createModel(final Platform platform) throws Exception {
+    Database db = null;
+    if (modulesDir == null) {
+      getLog()
+          .info("modulesDir for additional files not specified. Creating database with just Core.");
+      db = DatabaseUtils.readDatabaseWithoutConfigScript(getModel());
+    } else {
+      // We read model files using the filter, obtaining a file array. The models will be merged
+      // to create a final target model.
+      final Vector<File> dirs = new Vector<File>();
+      dirs.add(model);
+      final DirectoryScanner dirScanner = new DirectoryScanner();
+      dirScanner.setBasedir(new File(modulesDir));
+      final String[] dirFilterA = { dirFilter };
+      dirScanner.setIncludes(dirFilterA);
+      dirScanner.scan();
+      final String[] incDirs = dirScanner.getIncludedDirectories();
+      for (int j = 0; j < incDirs.length; j++) {
+        final File dirF = new File(modulesDir, incDirs[j]);
+        dirs.add(dirF);
+      }
+      final File[] fileArray = new File[dirs.size()];
+      for (int i = 0; i < dirs.size(); i++) {
+        fileArray[i] = dirs.get(i);
+      }
+
+      db = DatabaseUtils.readDatabaseWithoutConfigScript(fileArray);
+    }
+
+    // Create database
+    getLog().info("Executing creation script");
+    // crop database if needed
+    if (object != null) {
+      final Database empty = new Database();
+      empty.setName("empty");
+      db = DatabaseUtils.cropDatabase(empty, db, object);
+      getLog().info("for database object " + object);
+    } else {
+      getLog().info("for the complete database");
+    }
+
+    if (!platform.createTables(db, isDropfirst(), !isFailonerror())) {
+      throw new Exception(MSG_ERROR);
+    }
+    return db;
+  }
+
+  private void executePrescripts(final Platform platform) throws IOException {
+    executeSystemPreScript(platform);
+    executePreScript(platform);
+  }
+
+  private void executePostscript(final Platform platform) throws IOException {
+    // execute the post-script
+    if (getPostscript() == null) {
+      // try to execute the default prescript
+      final File fpost = new File(getModel(), "postscript-" + platform.getName() + ".sql");
+      if (fpost.exists()) {
+        getLog().info("Executing default postscript");
+        platform.evaluateBatch(DatabaseUtils.readFile(fpost), !isFailonerror());
+      }
+    } else {
+      platform.evaluateBatch(DatabaseUtils.readFile(getPostscript()), !isFailonerror());
     }
   }
 
@@ -272,6 +200,107 @@ public class CreateDatabase extends BaseDatabaseTask {
     if (postScript.exists()) {
       getLog().info("Executing script " + postScript.getName());
       platform.evaluateBatch(DatabaseUtils.readFile(postScript), !isFailonerror());
+    }
+  }
+
+  private void insertSourceData(final Platform platform, Database db)
+      throws IOException, Exception {
+    // Now we insert sourcedata into the database first we load the data files
+    final String folders = getInput();
+    final StringTokenizer strTokFol = new StringTokenizer(folders, ",");
+    final Vector<File> files = new Vector<File>();
+    while (strTokFol.hasMoreElements()) {
+      if (basedir == null) {
+        getLog().info("Basedir not specified, will insert just Core data files.");
+        final String folder = strTokFol.nextToken();
+        final File[] fileArray = DatabaseUtils.readFileArray(new File(folder));
+        for (int i = 0; i < fileArray.length; i++) {
+          files.add(fileArray[i]);
+        }
+      } else {
+        final String token = strTokFol.nextToken();
+        final DirectoryScanner dirScanner = new DirectoryScanner();
+        dirScanner.setBasedir(new File(basedir));
+        final String[] dirFilterA = { token };
+        dirScanner.setIncludes(dirFilterA);
+        dirScanner.scan();
+        final String[] incDirs = dirScanner.getIncludedDirectories();
+        for (int j = 0; j < incDirs.length; j++) {
+          final File dirFolder = new File(basedir, incDirs[j] + "/");
+          final File[] fileArray = DatabaseUtils.readFileArray(dirFolder);
+          for (int i = 0; i < fileArray.length; i++) {
+            files.add(fileArray[i]);
+          }
+        }
+      }
+    }
+
+    getLog().info("Disabling triggers");
+    Connection _connection = platform.borrowConnection();
+    platform.disableAllTriggers(_connection, db, false);
+    platform.returnConnection(_connection);
+
+    getLog().info("Inserting data into the database.");
+    // Now we insert the data into the database
+    final DatabaseDataIO dbdio = new DatabaseDataIO();
+    dbdio.setEnsureFKOrder(false);
+    dbdio.setUseBatchMode(true);
+    DataReader dataReader = dbdio.getConfiguredDataReader(platform, db);
+    dataReader.getSink().start();
+
+    for (int i = 0; i < files.size(); i++) {
+      getLog().debug("Importing data from file: " + files.get(i).getName());
+      dbdio.writeDataToDatabase(dataReader, files.get(i));
+    }
+
+    /*
+     * end method does final inserts when using batching, so call it after importing data files but
+     * before doing anything using those data (template, fk's, not null, ... )
+     */
+    dataReader.getSink().end();
+
+    DatabaseData dbData = new DatabaseData(db);
+    DatabaseUtils.readDataModuleInfo(db, dbData, basedir);
+    for (String template : DBSMOBUtil.getInstance().getSortedTemplates(dbData)) {
+      File configScript = new File(new File(modulesDir),
+          template + "/src-db/database/configScript.xml");
+      getLog().info("Loading config script for module from path " + configScript.getAbsolutePath());
+      if (configScript.exists()) {
+        final DatabaseIO dbIO = new DatabaseIO();
+        final Vector<Change> changesConfigScript = dbIO.readChanges(configScript);
+        platform.applyConfigScript(db, changesConfigScript);
+      } else {
+        getLog().error("Error. We couldn't find configuration script for template "
+            + configScript.getName() + ". Path: " + configScript.getAbsolutePath());
+      }
+    }
+
+    getLog().info("Executing onCreateDefault statements");
+    platform.executeOnCreateDefaultForMandatoryColumns(db, null);
+    getLog().info("Enabling notnull constraints");
+    platform.enableNOTNULLColumns(db);
+
+    boolean continueOnError = false;
+    getLog().info("Creating foreign keys");
+    boolean fksEnabled = platform.createAllFKs(db, continueOnError);
+
+    _connection = platform.borrowConnection();
+    getLog().info("Enabling triggers");
+    boolean triggersEnabled = platform.enableAllTriggers(_connection, db, false);
+    platform.returnConnection(_connection);
+
+    executePostScript(platform);
+
+    if (!triggersEnabled) {
+      getLog().error(
+          "Not all the triggers were correctly activated. The most likely cause of this is that the XML file of the trigger is not correct.");
+    }
+    if (!fksEnabled) {
+      getLog().error(
+          "Not all the foreign keys were correctly activated. Please review which ones were not, and fix the missing references.");
+    }
+    if (!triggersEnabled || !fksEnabled) {
+      throw new Exception(MSG_ERROR);
     }
   }
 
