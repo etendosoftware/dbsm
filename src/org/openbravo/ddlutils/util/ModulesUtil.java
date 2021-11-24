3d8ac3856b3615a35039a29681f488ed177d0ce9
Pablo Elena <pablo.elena@smfconsulting.es>
2021-11-24 13:25:36 -0300

    EPL-13: Update export task to support new modules directories when core in Jar

--- src/org/openbravo/ddlutils/util/ModulesUtil.java
+++ src/org/openbravo/ddlutils/util/ModulesUtil.java
@@ -19,7 +19,11 @@ public class ModulesUtil {
     public static String[] moduleDirs = new String[] {MODULES_BASE, MODULES_CORE};
 
     static {
-        checkCoreInSources(coreInSources());
+        try {
+            checkCoreInSources(coreInSources());
+        } catch (Exception e) {
+            System.out.println("Error initializing static block in dbsm ModulesUtil class." + e.getMessage());
+        }
     }
 
     public static Vector<File> get(String path) {
@@ -50,10 +54,13 @@ public class ModulesUtil {
         return dirs;
     }
 
-    public static void checkCoreInSources(Boolean isCoreInSources) {
+    public static void checkCoreInSources(Boolean isCoreInSources, String rootDir) {
         coreInSources = isCoreInSources;
         CoreMetadata coreData = null;
-        String workDir = getProjectRootDir();
+        String workDir = rootDir;
+        if (rootDir == null || rootDir.isBlank() || rootDir.isEmpty()) {
+            workDir = getProjectRootDir();
+        }
 
         if (isCoreInSources) {
             coreData = new SourceCoreMetadata(workDir);
@@ -64,13 +71,24 @@ public class ModulesUtil {
         moduleDirs = coreData.getAllModulesLocation();
     }
 
+    public static void checkCoreInSources(Boolean isCoreInSources) {
+        checkCoreInSources(isCoreInSources,"");
+    }
+
     /**
      * Verifies that the core is in sources
      * TODO: See if there is a more secure solution.
      * @return
      */
     public static Boolean coreInSources() {
-        String rootWorkDir = getProjectRootDir();
+        return coreInSources("");
+    }
+
+    public static Boolean coreInSources(String rootDir) {
+        String rootWorkDir = rootDir;
+        if (rootDir == null || rootDir.isBlank() || rootDir.isEmpty()) {
+            rootWorkDir = getProjectRootDir();
+        }
         File rootLocation = new File(rootWorkDir);
 
         // File used to verify that the core is in sources
@@ -84,9 +102,13 @@ public class ModulesUtil {
      * @return The root dir of the current project
      */
     public static String getProjectRootDir() {
-        return (String) EtendoPropertiesProvider.getInstance()
-                .getEtendoProperties()
-                .get("source.path");
+        String sourcePath = (String) EtendoPropertiesProvider.getInstance()
+                            .getEtendoProperties()
+                            .get("source.path");
+        if (sourcePath == null || sourcePath.isBlank() || sourcePath.isEmpty()) {
+            throw new IllegalArgumentException("The property 'source.path' is not defined.");
+        }
+        return sourcePath;
     }
 
     public static File[] union(File[] array1, File[] array2) {
